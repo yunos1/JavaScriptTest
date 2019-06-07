b3981526328acf111a3707efc972118a64cb5980
Liuqz <1562616404@qq.com>
2019-06-07 22:14:00 +0800

    test

---
+++ JobTest/vue_cli/vue-cli-demo/src/components/ComponentA.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div class="container">
+            <div class="con">
+                <p>this is conA</p>
+                <ComponentB />
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import ComponentB from '@/components/ComponentB'
+
+export default {
+    name:"ComponentA",
+    components:{
+        ComponentB
+    },
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        
+    },
+}
+</script>
+<style lang="less" scoped>
+    .container{
+        background: yellow;
+        .con{
+            p{
+                color: red;
+                font-size: 20px;
+            }
+        }
+    }
+</style>
---
+++ JobTest/vue_cli/vue-cli-demo/src/components/ComponentB.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(item,index) in list" :key="index">{{item}}</li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    name:"ComponentB",
+    data() {
+        return {
+            list:[1,2,3,4,5]
+        }
+    },
+    methods: {
+        
+    },
+}
+</script>
+<style lang="less" scoped>
+    ul{
+        border: 1px solid gray;
+        li{
+            list-style: none;
+            color: black;
+            font-size: 18px;
+        }
+    }
+</style>
